9fd51716d403609861c668380a15f8771c1854ef
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2023-10-16 15:02:32 -0500

    .............

--- app.js
+++ app.js
@@ -2,21 +2,29 @@ let menu = ["new", "list", "delete", "quit"]
 
 let opcion = prompt("De la lista de opciones, qué quieres hacer? \n" + menu[0] + '\n' + menu[1] + "\n" + menu[2] + '\n' + menu[3])
 
-let List = ["No tienes tareas por hacer, puedes agregar una nueva"]
+let listaInicial = "No tienes tareas por hacer, puedes agregar una nueva"
+let list = []
 
-function newTask() {
 
+if(opcion=="new"){
+  newTask(list)
 }
 
-function list(){
-
+function newTask(lista) {
+  let nuevaTarea = prompt("Introduce la nueva tarea");
+  lista.push(nuevaTarea)
+  console.log(nuevaTarea)
 }
 
+// function list(){
 
-function erase(){
+// }
 
-}
 
-function quit(){
+// function erase(){
 
-}
+// }
+
+// function quit(){
+
+// }
