1929535eb2d802f713f58240d575c62263eb341c
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2023-10-16 14:48:18 -0500

    Tarea Udemy-Ruta N

---
+++ app.js
@@ -0,0 +1,22 @@
+let menu = ["new", "list", "delete", "quit"]
+
+let opcion = prompt("De la lista de opciones, qué quieres hacer? \n" + menu[0] + '\n' + menu[1] + "\n" + menu[2] + '\n' + menu[3])
+
+let List = ["No tienes tareas por hacer, puedes agregar una nueva"]
+
+function newTask() {
+
+}
+
+function list(){
+
+}
+
+
+function erase(){
+
+}
+
+function quit(){
+
+}
